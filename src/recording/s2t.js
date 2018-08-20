bfa46a4ade1954ff01b46a9623d4c1435d9b621b
fliropp <grottum@gmail.com>
2018-08-20 11:23:04 +0200

    promise on return from speech analysis

--- src/recording/s2t.js
+++ src/recording/s2t.js
@@ -12,34 +12,35 @@ const client = new speech.SpeechClient();
 // Reads a local audio file and converts it to base64
 //const file = fs.readFileSync(fileName);
 module.exports = {
-  convert: (input) => {
-    //const audioBytes = input.toString('base64');
-    //const audioBytes = fs.createReadStream(input);//.pipe(recognizeStream);
+  convert: async (input) => {
+    return new Promise((resolve, reject) => {
+      const audio = {
+        content: input,
+      };
+      const config = {
+        encoding: 'LINEAR',
+        sampleRateHertz: 44100,
+        languageCode: 'en-US',
+      };
+      const request = {
+        audio: audio,
+        config: config,
+      };
 
-    const audio = {
-      content: input,
-    };
-    const config = {
-      encoding: 'LINEAR',
-      sampleRateHertz: 44100,
-      languageCode: 'en-US',
-    };
-    const request = {
-      audio: audio,
-      config: config,
-    };
-
-    client
-      .recognize(request)
-      .then(data => {
-        const response = data[0];
-        const transcription = response.results
-          .map(result => result.alternatives[0].transcript)
-          .join('\n');
-        console.log(`Transcription: ${transcription}`);
-      })
-      .catch(err => {
-        console.error('ERROR:', err);
+      client
+        .recognize(request)
+        .then(data => {
+          const response = data[0];
+          const transcription = response.results
+            .map(result => result.alternatives[0].transcript)
+            .join('\n');
+          console.log(`Transcription: ${transcription}`);
+          resolve(transcription);
+        })
+        .catch(err => {
+          console.error('ERROR:', err);
+          reject(Error(err));
+        });
       });
     }
 }
--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ const s2t = require('./recording/s2t.js')
 io.on('connection', (client) => {
   client.on('s2t-request', (audio) => {
     console.log('audio on server');
-    s2t.convert(audio);
-    client.emit('s2t-response', 'test-respone');
+    s2t.convert(audio)
+    .then((response) => {
+      client.emit('s2t-response', response);
+    });
   });
 });
 
